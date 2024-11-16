fd593c6c3508a5e19e151fc6f7f3302452e8e4b4
siddhesh-100 <sssiddheshhh@gmail.com>
2024-11-16 12:24:27 +0530

    added BFS.cpp

--- Codes/test.cpp
+++ Codes/test.cpp
@@ -2,12 +2,71 @@
 #include <vector>
 using namespace std;
 using vi = vector<int>;
+void bubbleSort(vi &v){
+    int n = v.size();
+    for(int i = n - 1; i > -1; i--){
+        for(int j = 0; j < i + 1; j++){
+            if(v[j] > v[j + 1]) swap(v[j], v[j + 1]);
+        }
+    }
+}
+void insertionSort(vi &v){
+    int n = v.size();
+    for(int i = 0; i < n; i++){
+        int j = i;
+        while(j > 0 && v[j] < v[j - 1]) {
+            swap(v[j], v[j - 1]);
+            j--;
+        }
+    }
+}
+void selectionSort(vi &v){
+    int n = v.size();
+    for(int i = 0; i < n - 1; i++){
+        int minId = i;
+        for(int j = i; j < n; j++){
+            if(v[j] < v[minId]) minId = j;
+        }
+        swap(v[i], v[minId]);
+    }
+}
+void merge(vi &v, int low, int mid, int high){
+    vi temp;
+    int left = low;
+    int right = mid + 1;
+    while(left <= mid && right <= high){
+        if(v[left] < v[right]){
+            temp.push_back(v[left]);
+            left++;
+        }
+        else{
+            temp.push_back(v[right]);
+            right++;
+        }
+    }
+    while(left <= mid){
+        temp.push_back(v[left]);
+        left++;
+    }
+    while(right <= high) {
+        temp.push_back(v[right]);
+        right++;
+    }
+    for(int i = low; i < high + 1; i++) v[i] = temp[i - low];
+}
+void ms(vi &v, int low, int high){
+    if(low == high) return;
+    int mid = (low + high)/2;
+    ms(v, low, mid);
+    ms(v, mid + 1, high);
+    merge(v, low, mid, high);
+}
 int partition(vi &v, int low, int high){
-    int piv = v[low];
     int i = low, j = high;
+    int piv = v[i];
     while(i < j){
         while(v[i] <= piv && i < high) i++;
-        while(v[j] > piv && j > low) j--; 
+        while(v[j] > piv && j > low) j--;
         if(i < j) swap(v[i], v[j]);
     }
     swap(v[low], v[j]);
@@ -22,7 +81,7 @@ void qs(vi &v, int low, int high){
 }
 void print(vi &v){
     for(const auto &i : v) cout << i << " ";
-    cout << endl;
+    cout << "\n";
 }
 int main(){
     ios_base::sync_with_stdio(false);
