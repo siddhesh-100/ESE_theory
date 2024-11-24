9aee685ec97a7576a3b69fccda18cc4a5cc29df5
siddhesh-100 <sssiddheshhh@gmail.com>
2024-11-25 01:45:36 +0530

    Added BFS.cpp

--- Lab/Exp_7/BFS.cpp
+++ Lab/Exp_7/BFS.cpp
@@ -2,42 +2,38 @@
 #include <vector>
 #include <queue>
 using namespace std;
-void BFS(vector<int> v[], int n, int start){
-    vector<bool> vis(n, false);
-    queue<int> q;
-
-    vis[start] = true;
-    q.push(start);
-
-    while(!q.empty()){
+void BFS(int start, vector<int> graph[], vector<bool>& visited) {
+    queue<int> q; 
+    visited[start] = true; 
+    q.push(start); 
+    cout << "BFS Traversal starting from node " << start + 1 << ": ";
+    while (!q.empty()) {
         int node = q.front();
         q.pop();
-        cout << node << " ";
-
-        for(auto i : v[node]){
-            if(!vis[i]){
-                vis[i] = true;
+        cout << node + 1 << " ";
+        for (int i : graph[node]) {
+            if (!visited[i]) {
+                visited[i] = true;
                 q.push(i);
             }
         }
     }
     cout << endl;
 }
-int main(){
+int main() {
     int n, m;
     cout << "Enter the number of nodes and edges: ";
     cin >> n >> m;
-
     vector<int> graph[n];
-    cout << "Enter the edges in form of a pair of nodes\n";
-    for(int i = 0; i < m; i++){
-        int u, v; cin >> u >> v;
+    cout << "Enter the edges in form of a pair of nodes (u, v):\n";
+    for (int i = 0; i < m; i++) {
+        int u, v; cin >> u >> v; u--; v--;
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
-    int start; 
     cout << "Enter the node to start the BFS traversal: ";
-    cin >> start;
-    BFS(graph, n, start);
+    int start; cin >> start; start--;
+    vector<bool> visited(n, false);
+    BFS(start, graph, visited);
     return 0;
 }
